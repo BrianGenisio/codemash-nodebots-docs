7b2c3c2a5f0b993f0dca79baddf6a458aa46d00d
Brian Genisio <BrianGenisio@Gmail.com>
2014-12-07 22:33:03 -0500

    adding a board test to go with the physical testers I built

--- examples/board_test.js
+++ examples/board_test.js
@@ -3,8 +3,34 @@ var board = new five.Board();
 
 board.on("ready", function() {
 
-  (new five.Sensor("A7")).on("data", function() {
-    console.log(this.value);
+  var analogLow = five.Sensor({pin: "A0", freq: 500});
+  var analogHigh = five.Sensor({pin: "A5", freq: 500});
+
+  var led = five.Led(13);
+  var servo = five.Servo(11);
+  
+
+  var lowData = analogLow.on("data", function() {
+    console.log("LOW:  ", this.value);
   });
+
+  var highData = analogHigh.on("data", function() {
+    console.log("HIGH: ", this.value);
+  });
+
+  led.blink();
+  servo.sweep();
+
+  setTimeout(function() {
+    lowData.removeAllListeners();
+    highData.removeAllListeners();
+    led.stop().off();
+    servo.stop().center();
+
+    setTimeout(function() {
+      process.exit();
+    }, 500);
+    
+  }, 4000);
 
 });
