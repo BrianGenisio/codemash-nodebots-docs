1c36288f1f5cbacc8eca5ab047afe1ce68226cee
Jeff Yates <jeff.yates@alumni.manchester.ac.uk>
2014-12-19 11:44:55 -0500

    Moved stdin setup and removed repl hint

--- examples/base-line-follower.js
+++ examples/base-line-follower.js
@@ -1,12 +1,11 @@
 var five = require("johnny-five");
-var fs = require("fs");
 var board = new five.Board();
 
+var stdin = process.stdin;
+stdin.setRawMode(true);
+stdin.resume();
+
 board.on("ready", function () {
-    var stdin = process.stdin;
-    stdin.setRawMode(true);
-    stdin.resume();
-    
     var wheels = {
         left: new five.Servo({ pin: 9, type: 'continuous' }),
         right: new five.Servo({ pin: 10, type: 'continuous' }),
@@ -17,7 +16,7 @@ board.on("ready", function () {
         goForward: function () {
             wheels.left.ccw();
             wheels.right.cw();
-	        console.log("goForward");
+            console.log("goForward");
         },
         turnLeft: function () {
             wheels.left.center();
@@ -30,7 +29,7 @@ board.on("ready", function () {
             console.log("turnRight");
         }
     };
-	wheels.completeStop();
+    wheels.completeStop();
     
     var calibrating = true;
     var eyes = new five.IR.Reflect.Array({
@@ -52,36 +51,33 @@ board.on("ready", function () {
                 // Stop calibration
                 calibrating = false;
                 
-                setImmediate(function() {
-	                console.log("Place the bot on the course and press enter to begin line following...");
-	                stdin.once("keypress", function() {
-		                // Start watching the line and driving
-		                eyes.on("line", function(err,line) {
-			                if (line < 250) {
-				                wheels.turnRight();
-			                } else if (line > 4750) {
-				                wheels.turnLeft();
-			                } else {
-				                wheels.goForward();
-			                }
-			                console.log(line);
-		                });
-
-		                setImmediate(function() {
-			                stdin.once("keypress", function() {
-				                // Stop the bot and quit
-				                eyes.removeAllListeners();
-				                wheels.completeStop();
-				                // Need to give time for the signals to get to the wheels
-				                setTimeout(function() { process.exit(0); }, 250);
-			                });
-		                });
-	                });
+                setImmediate(function () {
+                    console.log("Place the bot on the course and press enter to begin line following...");
+                    stdin.once("keypress", function () {
+                        // Start watching the line and driving
+                        eyes.on("line", function (err, line) {
+                            if (line < 1000) {
+                                wheels.turnRight();
+                            } else if (line > 4000) {
+                                wheels.turnLeft();
+                            } else {
+                                wheels.goForward();
+                            }
+                            console.log(line);
+                        });
+                        
+                        setImmediate(function () {
+                            stdin.once("keypress", function () {
+                                // Stop the bot and quit
+                                eyes.removeAllListeners();
+                                wheels.completeStop();
+                                // Need to give time for the signals to get to the wheels
+                                setTimeout(function () { process.exit(0); }, 250);
+                            });
+                        });
+                    });
                 });
             });
         });
     });
-
-    // Include this line to have access to the components at the command line
-    //    this.repl.inject({ wheels: wheels, eyes: eyes });
 });
