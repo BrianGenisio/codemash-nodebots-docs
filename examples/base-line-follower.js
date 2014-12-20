dfc31b6d5e18c4b7359a3cc60e4225fcbcff1cc2
Jeff Yates <jeff.yates@alumni.manchester.ac.uk>
2014-12-19 21:22:50 -0500

    Refactored to toggling on keypress

--- examples/base-line-follower.js
+++ examples/base-line-follower.js
@@ -29,52 +29,44 @@ board.on("ready", function () {
             console.log("turnRight");
         }
     };
-    wheels.stop();
     
-    var calibrating = true;
     var eyes = new five.IR.Reflect.Array({
         emitter: 13,
         pins: ["A0", "A1", "A2", "A3", "A4", "A5"]
     });
-    eyes.enable();
     
-    stdin.once("keypress", function () {
-        // Start calibration
-        // All sensors need to see the extremes so they can understand what a line is,
-        // so move the eyes over the materials that represent lines and not lines during calibration.
-        eyes.calibrateUntil(function () { return !calibrating; });
+    var calibrating = true;
+    var running = false;
+
+    wheels.stop();
+    
+    // Start calibration
+    // All sensors need to see the extremes so they can understand what a line is,
+    // so move the eyes over the materials that represent lines and not lines during calibration.
+    eyes.calibrateUntil(function () { return !calibrating; });
+    
+    stdin.on("keypress", function() {
+        calibrating = false;
+        running = !running;
         
-        console.log("When calibration is complete, hit enter");
-        setImmediate(function () {
-            stdin.once("keypress", function () {
-                // Stop calibration
-                calibrating = false;
-                
-                setImmediate(function () {
-                    console.log("Place the bot on the course and press enter to begin line following...");
-                    stdin.once("keypress", function () {
-                        // Start watching the line and driving
-                        eyes.on("line", function (err, line) {
-                            if (line < 1000) {
-                                wheels.pivotRight();
-                            } else if (line > 4000) {
-                                wheels.pivotLeft();
-                            } else {
-                                wheels.forward();
-                            }
-                            console.log(line);
-                        });
-                        
-                        setImmediate(function () {
-                            stdin.once("keypress", function () {
-                                // Stop the bot and quit
-                                eyes.removeAllListeners();
-                                wheels.stop();
-                            });
-                        });
-                    });
-                });
-            });
-        });
+        if (!running) {
+            wheels.stop();
+            console.log("Stopped running. Press any key to start again...")
+        }
     });
+
+    eyes.on("line", function(err, line) {
+        if(!running) return;
+    
+        if (line < 1000) {
+            wheels.pivotRight();
+        } else if (line > 4000) {
+            wheels.pivotLeft();
+        } else {
+            wheels.forward();
+        }
+        console.log(line);
+    });
+    
+    eyes.enable();
 });
